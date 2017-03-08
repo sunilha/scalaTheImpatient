e2bd3e851c86100d0562b79e4dddca430358dd1d
sunilha <sunil.ha@gmail.com>
2017-03-07 18:04:21 -0800

    adding skeleton code

---
+++ Chapter1/src/test/java/com/scala/chapter1/BaseTest.scala
@@ -0,0 +1,9 @@
+package com.scala.chapter1
+
+/**
+  * User: sunilkumar.ashok
+  * Date: 3/7/2017
+  */
+class BaseTest {
+
+}
---
+++ Chapter1/src/test/java/com/scala/chapter1/SampleTest.scala
@@ -0,0 +1,11 @@
+package com.scala.chapter1
+
+import org.scalatest.FunSuite
+
+/**
+  * User: sunilkumar.ashok
+  * Date: 3/7/2017
+  */
+class SampleTest extends FunSuite{
+
+}
